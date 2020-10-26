989027bfafbab291d20592a815f4fd883903634e
Malik Bougacha <bougacha@google.com>
2020-10-26 14:51:32 +0100

    reformat python file with black

--- api/app/endpoints/cm.py
+++ api/app/endpoints/cm.py
@@ -5,9 +5,9 @@ import os
 api = Namespace("cm", "Calculation module endpoint")
 current_file_dir = os.path.dirname(os.path.abspath(__file__))
 
+
 @api.route("/<int:id_cm>/task/<int:task_id>")
 class CM_fakeoutput(Resource):
     def get(self, id_cm, task_id):
-        fakeoutput = os.path.join(current_file_dir, 'fakeoutput.json')
+        fakeoutput = os.path.join(current_file_dir, "fakeoutput.json")
         return send_file(fakeoutput)
-
