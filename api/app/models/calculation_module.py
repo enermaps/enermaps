aca3ee1e2e10b0cfe8e566dd3abdd9659e4f31ab
Malik Bougacha <bougacha@google.com>
2020-11-13 14:04:37 +0100

    fixes on the calculation modules.
    When redis is not available, return an empty list of calculation
    modules.
    Correct the exposition of the calculation modules.

--- api/app/models/calculation_module.py
+++ api/app/models/calculation_module.py
@@ -1,6 +1,13 @@
 import re
 import json
 from celery import Celery, result
+import logging
+import redis
+import json
+
+import kombu
+from celery import Celery
+import celery
 
 
 # TODO; migrate this to pyparsing maybe ?
@@ -8,7 +15,7 @@ INFO_STRING = re.compile("^(?P<task_id>[a-zA-Z._]+) \\[cm_info=(?P<task_info>.+)
 
 
 def get_celery_app():
-    app = Celery(broker="redis://guest@localhost//", backend="redis://localhost")
+    app = Celery(broker="redis://guest@redis//", backend="redis://redis")
     app.conf.update(
         task_serializer="json",
         accept_content=["json"],  # Ignore other content
@@ -16,6 +23,12 @@ def get_celery_app():
         timezone="Europe/Zurich",
         enable_utc=True,
     )
+    transport_options = {}
+    transport_options['max_retries'] = 3
+    transport_options['interval_start'] = 0.5
+    transport_options['interval_step'] = 1
+    transport_options['interval_max'] = 2
+    app.conf.update(broker_transport_options=transport_options)
     return app
 
 
@@ -27,15 +40,19 @@ def task_by_id(task_id, cm_name):
 
 def list_cms():
     app = get_celery_app()
-    app_inspector = app.control.inspect()
-    nodes = app_inspector.registered("cm_info")
-    print(nodes)
+    try:
+        app_inspector = app.control.inspect()
+        nodes = app_inspector.registered("cm_info")
+    except (redis.exceptions.ConnectionError, kombu.exceptions.OperationalError) as err:
+        # If redis is down, we just don't expose any calculation module
+        logging.error("Connection to celery broken resulted in an error: ", err)
+        return {}
     if not nodes:
         nodes = {}
     cms = {}
     for node in nodes.values():
         for entry in node:
-            task = from_registration_string(entry, app)
+            task = from_registration_string(entry)
             cms[task.task_id] = task
     return cms
 
