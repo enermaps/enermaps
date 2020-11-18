d0b63d73fe279be8447b4349f5028b3526578fad
Malik Bougacha <bougacha@google.com>
2020-11-18 15:54:11 +0100

    fix the worker to start correctly

--- cm/multiply/worker.py
+++ cm/multiply/worker.py
@@ -2,8 +2,8 @@ import json
 import inspect
 import marshmallow
 from celery import Celery, Task
-from multiply_raster import MultiplyRasterstats
-import IPython
+from celery.worker import worker
+#from multiply_raster import MultiplyRasterstats
 
 app = Celery(__name__, broker="redis://guest@localhost//", backend="redis://localhost")
 
@@ -29,13 +29,6 @@ class BaseTask(Task):
         d["doc"] = self.__doc__
         return json.dumps(d)
 
-# used CMSchema is the next step
-class CMSchema(marshmallow.Schema):
-    rasters = marshmallow.fields.List(marshmallow.fields.String)
-    #this could be a bit more restrictive
-    selection = marshmallow.fields.Mapping() #GEOJson
-    scale_factor = marshmallow.fields.Integer()
-
 @app.task(base=BaseTask)
 def MultiplyRaster():
     """This is a calculation module that multiplies the raster by an factor.
@@ -44,8 +37,5 @@ def MultiplyRaster():
     return val_multiply
 
 if __name__ == "__main__":
-    app.worker_main()
-    #print(help(app))
-    #import time
-    #time.sleep(400)
-    #IPython.embed()
+    w = worker.WorkController(app=app)
+    w.start()
