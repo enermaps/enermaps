5c05a65abbbad01d27171c6aecbe5ea6e1c5e483
Malik Bougacha <bougacha@google.com>
2020-11-24 13:41:14 +0100

    only fetch once the initial dataset
    This uses the gunicorn init hook as a way of running the
    dataset intialisation once upon startup. The number of
    worker can now be any number, the initialisation will only run
    once.
    This is achived by creating the application once which allow the
    app creation function to be called and check for its environment.

---
+++ api/gunicorn.py
@@ -0,0 +1,21 @@
+"""Configuration file for gunicorn, As we are using init hooks,
+we need to keep this file in python (see
+https://github.com/benoitc/gunicorn/blob/master/examples/example_config.py)
+for an example.
+"""
+import sys
+
+from app import create_app
+
+
+def on_starting(_):
+    """This is a gunicorn hook that is run once upon starting the server.
+    We use this hook to setup the initial data creation, right now those are
+    run upon app creation, so we just create a dummy application to trigger
+    those hooks
+    """
+    print("Running the startup hook ", flush=True)
+    create_app()
+    print("startup hook finished")
+    #  Explicitely flush so that we have the init logs
+    sys.stdout.flush()
