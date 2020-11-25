c0793e282bef28912432ec725be10e03abbb7ab3
Cedric Serugendo <cedric.serugendo@crem.ch>
2020-11-25 18:56:20 +0100

    adding test

--- cm/multiply/test.py
+++ cm/multiply/test.py
@@ -1,4 +1,5 @@
 import json
+import os
 import unittest
 
 from rasterstats import zonal_stats
@@ -9,14 +10,29 @@ from multiply_raster import MultiplyRasterStats
 class TestCM(unittest.TestCase):
     def test_multiply_raster_stats(self):
         """Testing to multiply the raster by a factor."""
+
+        def get_testdata_path(filename):
+            """Return the absolute path of the filename.
+            """
+            #current_file_dir = os.path.dirname(os.path.abspath(os.getcwd()))
+            #testdata_dir = os.path.join(os.path.dirname(current_file_dir), "testdata")
+            #print('path')
+            #print(os.getcwd())
+            #print(os.path.abspath(os.getcwd()))
+            #print(os.path.join(testdata_dir, filename))
+            os.path.join('.', 'testdata', filename)
+            return os.path.join('.', 'testdata', filename)
+
         factor = 2
-        test_geojson = "selection_shapefile.geojson"
-        with open(test_geojson) as fd:
+        selection_path = get_testdata_path("switzerland.geojson")
+        raster = get_testdata_path("GeoTIFF_test.tif")
+        #selection_path = os.path.join('.', 'testdata', filemane)
+        with open(selection_path) as fd:
             selection = json.load(fd)
-        val_double = MultiplyRasterStats(selection, "GeoTIFF_test.tif", factor)
+        val_double = MultiplyRasterStats(selection, raster, factor)
         stats = zonal_stats(
-            test_geojson,
-            "GeoTIFF_test.tif",
+            selection,
+            raster,
             stats="count min mean max",
         )
         for key, value in stats[0].items():
