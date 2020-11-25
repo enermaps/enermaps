82dcbe1bf4f877991e889f5ad19a4f721965ef7b
Malik Bougacha <bougacha@google.com>
2020-11-25 12:34:07 +0100

    add CMResult

---
+++ frontend/src/src/CMResult.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { onMount } from 'svelte';
+	export let cm_task;
+	let cm_status;
+
+	onMount(async() => {
+		getTaskResult();
+	});
+	async function getTaskResult() {
+		const result = await fetch('/api/cm/' + cm_task.cm.name + '/task/' + cm_task.task_id);
+		cm_status = await result.json();
+		console.log(cm_status);
+		cm_status = cm_status;
+	}
+</script>
+
+task_id: {cm_task.task_id}
+task_name: {cm_task.cm.name}
+status: {JSON.stringify(cm_status)}
