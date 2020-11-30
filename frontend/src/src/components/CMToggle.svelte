c5901e0b8daad9321b47c8c24938ed4e865ec797
Malik Bougacha <bougacha@google.com>
2020-11-30 16:07:08 +0100

    center the cmbutton icon

--- frontend/src/src/components/CMToggle.svelte
+++ frontend/src/src/components/CMToggle.svelte
@@ -6,21 +6,23 @@ function toggleCM() {
 </script>
 <style>
 #cm_toggle {
-  background: url('../analytics_icon.png');
-  background-repeat: no-repeat;
-  background-size: cover;
-  height: 30px;
-  width: 30px;
-  display: inline-block;
+  height: 100%;
+  width: 100%;
+  cursor: pointer;
+}
+#cm_toggle_container:hover {
+  background-color: #f4f4f4;
+  background: url('../analytics_icon.png') #fafafa;
 }
 #cm_toggle_container {
+  background-repeat: no-repeat;
+  background-size: cover;
+  background: url('../analytics_icon.png');
   height: 30px;
   width: 30px;
   background-color: white;
   padding: 4px;
   border-radius: 4px;
-  background-clip: padding-box;
-  cursor: auto;
   border: 2px solid rgba(0,0,0,0.2);
 }
 </style>
