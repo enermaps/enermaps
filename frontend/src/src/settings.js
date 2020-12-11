b30a01633d04c877e78b75f1fe1b36cee2ad7a62
Malik Bougacha <bougacha@google.com>
2020-12-11 12:25:24 +0100

    fix settings import

--- frontend/src/src/settings.js
+++ frontend/src/src/settings.js
@@ -2,7 +2,7 @@ export const BASE_URL = '/';
 export const INITIAL_MAP_CENTER = [51.505, -0.09];
 export const INITIAL_ZOOM = 13;
 export const BASE_LAYER_URL =
-'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png';
+  'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png';
 export const BASE_LAYER_PARAMS= {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">'+
     'OpenStreetMap</a> &copy; <a href="https://cartodb.com/attributions">CartoDB</a>',
