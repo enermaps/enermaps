c4b34ed5b5621ad60adece71dc0d2a323a9b813b
Malik Bougacha <bougacha@google.com>
2020-12-11 18:13:13 +0100

    fix base url

--- frontend/src/src/settings.js
+++ frontend/src/src/settings.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://127.0.0.1:7000/';
+export const BASE_URL = '/';
 export const INITIAL_MAP_CENTER = [51.505, -0.09];
 export const INITIAL_ZOOM = 13;
 export const BASE_LAYER_URL =
