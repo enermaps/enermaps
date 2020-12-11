315e006c575ee8255cb098e04c85edda302e5cf3
Malik Bougacha <bougacha@google.com>
2020-12-11 16:13:17 +0100

    fix the imports

--- frontend/src/src/components/Chart.svelte
+++ frontend/src/src/components/Chart.svelte
@@ -1,31 +1,33 @@
 <script>
     import { onMount } from 'svelte';
-    import Chart from 'chart.js';
+    import Chart from 'chartjs';
     export let data;
     export let labels;
-    let chart_element;
+    let chartElement;
     let chart;
 
     async function createChart() {
-        const myChart = new Chart(chart_element, {
-            type: 'scatter',
-            data: {
-                datasets: [{
-                label: 'Raster statistic',
-                data: data}]
-            },
-            options: {
-                scales: {
-                    xAxes: [{
-                    type: 'category',
-                    labels: labels
-                    }]
-                }
-            }
-        });
-        chart = myChart;
+      let ctx = chartElement.getContext('2d')
+      let myChart = new Chart(ctx, {
+          type: 'scatter',
+          data: {
+              datasets: [{
+              label: 'Raster statistic',
+              data: data}]
+          },
+          options: {
+              scales: {
+                  xAxes: [{
+                  type: 'category',
+                  labels: labels
+                  }]
+              }
+          }
+      });
+      chart = myChart;
+      console.log(chart, data, labels);
     }
     onMount(createChart);
 </script>
 
-<canvas bind:this={chart_element}></canvas>
+<canvas bind:this={chartElement}></canvas>
