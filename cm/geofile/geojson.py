ffe73ea0a12d3268eef4c47d7b13dd386d51142e
cedricserugendo <cedric.serugendo@crem.ch>
2021-01-15 21:51:41 +0100

    generate id with uuid module

--- cm/geofile/geojson.py
+++ cm/geofile/geojson.py
@@ -1,6 +1,5 @@
 import os
-import random
-import string
+import uuid
 
 import requests
 
@@ -11,16 +10,10 @@ TESTDATA_DIR = os.path.join(CURRENT_DIR, "testdata")
 TEST_FILE = os.path.join(TESTDATA_DIR, "small_test.tif")
 
 
-def get_random_string(length):
-    letters = string.ascii_lowercase
-    random_id = "".join(random.choice(letters) for i in range(length))
-    return random_id
-
-
 def post_geofile(file):
     files = {
         "file": (
-            "frontend_name_" + get_random_string(10) + ".tif",
+            "frontend_name_" + str(uuid.uuid1()) + ".tif",
             open(file, "rb"),
             "image/geotiff",
         ),
