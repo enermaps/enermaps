e44eeba5800c143f93813ab81a136dfaf767c6a5
cedricserugendo <cedric.serugendo@crem.ch>
2021-01-19 16:30:15 +0100

    clean up the code

--- cm/geofile/geojson.py
+++ cm/geofile/geojson.py
@@ -1,27 +1,16 @@
 import logging
 import uuid
+
 from requests.exceptions import ConnectionError
+
 from BaseCM.cm_output import output_raster
-
-
-def post_geofile(file):
-    files = {
-        "file": (
-            "frontend_name_" + str(uuid.uuid1()) + ".tif",
-            open(file, "rb"),
-            "image/geotiff",
-        ),
-    }
-    try:
-        response = requests.post("http://127.0.0.1:7000/api/geofile/", files=files)
-        print(response.status_code)
-    except :
-        print("ERROR in the post.")
 
 
 def process(file):
     try:
-        status = output_raster("frontend_name_" + str(uuid.uuid1()) + ".tif", open(file, "rb"))
+        status = output_raster(
+            "frontend_name_" + str(uuid.uuid1()) + ".tif", open(file, "rb")
+        )
         return status
     except ConnectionError:
         logging.error("Error during the post of the file.")
--- cm/geofile/worker.py
+++ cm/geofile/worker.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import BaseCM.cm_base as cm_base
-
 from geojson import process
 
 app = cm_base.get_default_app()
@@ -10,8 +9,8 @@ schema_path = cm_base.get_default_schema_path()
 @app.task(base=cm_base.CMBase, bind=True, schema_path=schema_path)
 def new_cm(self, file: str, params: dict):
     """New cm to test the integration work flow"""
-    geofile_output = process(file)
-    return geofile_output
+    status = process(file)
+    return status
 
 
 if __name__ == "__main__":
