f0527daa62d2acb46cc83559d99c5d72f854cd8b
cedricserugendo <cedric.serugendo@crem.ch>
2021-02-04 11:42:59 +0100

    put the try/expect into output_raster function

--- cm/base/BaseCM/cm_output.py
+++ cm/base/BaseCM/cm_output.py
@@ -1,7 +1,10 @@
+import logging
 import os
 from typing import Dict
 
 import requests
+from requests.exceptions import ConnectionError
+
 from marshmallow import Schema, fields
 from marshmallow_union import Union
 
@@ -72,5 +75,9 @@ API_URL = os.environ.get("API_URL")
 def output_raster(raster_name, raster_fd):
     """Add a raster to the api"""
     files = {"file": (raster_name, raster_fd, "image/tiff")}
-    resp = requests.post(API_URL + "api/geofile/", files=files)
-    return resp.ok
+    try:
+        resp = requests.post(API_URL + "api/geofile/", files=files)
+        return resp.ok
+    except ConnectionError:
+        logging.error("Error during the post of the file.")
+        return False
