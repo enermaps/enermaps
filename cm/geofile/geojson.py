20130a257a0958dfaf99ee8191b9dabfffb5c313
cedricserugendo <cedric.serugendo@crem.ch>
2021-02-04 13:37:01 +0100

    add information on how to post geofile

--- cm/geofile/geojson.py
+++ cm/geofile/geojson.py
@@ -1,16 +1,11 @@
-import logging
 import uuid
 
 from BaseCM.cm_output import output_raster
-from requests.exceptions import ConnectionError
 
 
 def process(file):
-    try:
-        status = output_raster(
-            "frontend_name_" + str(uuid.uuid1()) + ".tif", open(file, "rb")
-        )
-        return status
-    except ConnectionError:
-        logging.error("Error during the post of the file.")
-        return False
+    status = output_raster(
+        "frontend_name_" + str(uuid.uuid1()) + ".tif", open(file, "rb")
+    )
+    return status
+
