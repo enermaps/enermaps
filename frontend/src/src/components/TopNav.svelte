9fa2213e64defc468b918bcc2ecc67a1009a8e4b
melhk <melanie.huck@protonmail.ch>
2021-02-25 16:41:25 +0100

    Adding logo and title to top bar

--- frontend/src/src/components/TopNav.svelte
+++ frontend/src/src/components/TopNav.svelte
@@ -1,44 +1,86 @@
-<script>
-    import {onMount} from 'svelte';
+<script context="module">
+  import 'leaflet-search/dist/leaflet-search.src.js';
+  import 'leaflet-search/dist/leaflet-search.src.css';
+
+
+  //import {onMount} from 'svelte';
+
+  //let topnav;
+
+  /*
+  onMount(async () => {
+
+    topnav.addControl(makeSearchControl());
+  });
+  */
+ 
+  export function makeSearchControl() {
+  const searchControl = new L.Control.Search({
+    url: 'https://nominatim.openstreetmap.org/search?format=json&q={s}',
+    jsonpParam: 'json_callback',
+    propertyName: 'display_name',
+    propertyLoc: ['lat', 'lon'],
+    marker: false, // L.circleMarker([0, 0], { radius: 30 }),
+    autoCollapse: true,
+    autoType: false,
+    minLength: 2,
+    container: "map_control"
+  });
+  return searchControl;
+}
+
 </script>
 
 <style>
 
 /* Add a black background color to the top navigation */
-.topnav {
+
+#topnav {
   background-color: #27275b;
   overflow: hidden;
+  padding: 8px 20px;
 }
+
 
 /* Style the links inside the navigation bar */
 
-.topnav div {
-  float: left;
+#topnav div {
   color: #eff4fa;
   text-align: center;
-  padding: 10px 20px;
   text-decoration: none;
-  font-size: 17px;
+  font-size: 32px;
+  box-sizing: border-box;
+  vertical-align: middle;
+  display: inline-block;
+}
+
+
+#logo {
+  box-sizing: border-box;
+  height: 100%;
+}
+
+#title {
+  display: inline-block;
+  vertical-align: middle;
+  box-sizing: border-box;
+}
+
+img {
+  vertical-align: middle;
+  display: inline-block;
+  height: 40px;
   box-sizing: border-box;
 }
 
 
-/* Change the color of links on hover */
-/*
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
-*/
-/* Add a color to the active/current link */
-/*
-.topnav a.active {
-  background-color: #4CAF50;
-  color: white;
-}
-*/
+
+
 </style>
 
-<div class="topnav">
-    <div href="#EnerMaps">EnerMaps</div>
-  </div> 
+<div id="topnav">
+  <div id='logo'> <img src='images/logo.png' alt='EnerMaps'> </div>
+  <span></span>
+  <div id='title'>EnerMaps</div>
+  <div id='map_control'> </div>
+</div> 
