ad65306c858eae501c52b78f4e6ec743123473f7
Giuseppe PERONATO <giuseppe.peronato@idiap.ch>
2021-05-06 17:52:49 +0200

    init

---
+++ cm/heatlearn/test.py
@@ -0,0 +1,24 @@
+import json
+import os
+import unittest
+
+CURRENT_FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_testdata_path(filename):
+    """Return the absolute path of the filename."""
+    return os.path.join(CURRENT_FILE_DIR, "testdata", filename)
+
+
+def load_geojson(test_filename):
+    test_geojson = get_testdata_path(test_filename)
+    with open(test_geojson) as fd:
+        return json.load(fd)
+
+
+class TestCM(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ cm/heatlearn/worker.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+from BaseCM import cm_base as cm_base
+from BaseCM import cm_input as cm_input
+from multiply_raster import rasterstats
+
+app = cm_base.get_default_app()
+schema_path = cm_base.get_default_schema_path()
+
+
+@app.task(base=cm_base.CMBase, bind=True, schema_path=schema_path)
+def multiply_raster(self, selection: dict, rasters: list, params: dict):
+    """This is a calculation module that multiplies the raster by an factor.
+    If there is no raster, we raise a value error.
+    If there are many rasters, we select the first one.
+    """
+    # def create_data_indicator_str(json_form):
+    #    for
+    #    return data_indicator
+
+    if not rasters:
+        raise ValueError("Raster list must be non-empty.")
+    if "features" not in selection:
+        raise ValueError("The selection must be a feature set.")
+    if not selection["features"]:
+        raise ValueError("The selection must be non-empty.")
+    raster_path = cm_input.get_raster_path(rasters[0])
+    self.validate_params(params)
+    factor = params["factor"]
+    val_multiply = rasterstats(selection, raster_path, factor)
+    return val_multiply
+
+
+if __name__ == "__main__":
+    cm_base.start_app(app)
