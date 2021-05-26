0c8db4dbd651e296811385ec62a7c08c30ad432d
Giuseppe PERONATO <giuseppe.peronato@idiap.ch>
2021-05-26 12:27:48 +0200

    first sample implementation with single raster tile

--- cm/base/BaseCM/cm_output.py
+++ cm/base/BaseCM/cm_output.py
@@ -80,7 +80,6 @@ def validate(output: Dict) -> Dict:
     # data structure
     return output_schema.load(data=output)
 
-
 def output_raster(raster_name, raster_fd):
     """Add a raster to the api."""
     files = {"file": (raster_name, raster_fd, "image/tiff")}
