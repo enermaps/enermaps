f375f2ae2f8d54dd011ee08dc9fa0feb07cf44c1
Flavio Tarsetti <flavio.tarsetti@idiap.ch>
2021-06-08 15:47:21 +0200

    Revert "Moving search bar into the top nav"
    This reverts commit 9a5915e31ccfe7999e9a0a56c00c49b9fa3cb329.

--- frontend/src/src/components/Map.svelte
+++ frontend/src/src/components/Map.svelte
@@ -20,7 +20,6 @@ import 'leaflet-search/dist/leaflet-search.src.css';
 import AreaSelection from './AreaSelection.svelte';
 import LayerSelection from './LayerSelection.svelte';
 import CMToggle from './CMToggle.svelte';
-import TopNav from './TopNav.svelte';
 
 import {activeOverlayLayersStore, activeSelectionLayerStore} from '../stores.js';
 
@@ -136,7 +135,7 @@ function makeCMToggleControl() {
 // https://github.com/stefanocudini/leaflet-search
 function makeSearchControl() {
   const searchControl = new L.Control.Search({
-    container : 'findbox',
+    position : 'topleft',
     url: 'https://nominatim.openstreetmap.org/search?format=json&q={s}',
     jsonpParam: 'json_callback',
     propertyName: 'display_name',
@@ -159,41 +158,14 @@ function makeSearchControl() {
 
 <style>
 
-#page {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  box-sizing: border-box;
-  flex-direction: column;
-}
-
 #map {
   width: 100%;
   height: 100%;
-  display: flex;
   box-sizing: border-box;
 }
-
-#findbox {
-  display: inline-block;
-  overflow: visible;
-  vertical-align: middle;
-}
-
-/*
-.search-tooltip {
-	width: 200px;
-}
-.leaflet-control-search  {
-  background-color: tomato;
-}
-*/
 
 </style>
 
 <svelte:window on:resize={resizeMap} />
 
-<div id="page">
-<TopNav><div id="findbox"> </div></TopNav>
 <div id="map"></div>
-</div>
--- frontend/src/src/components/TopNav.svelte
+++ frontend/src/src/components/TopNav.svelte
@@ -1,16 +1,48 @@
 <script context="module">
   import 'leaflet-search/dist/leaflet-search.src.js';
   import 'leaflet-search/dist/leaflet-search.src.css';
+
+  //import CMToggle from './CMToggle.svelte';
+
+  // import {onMount} from 'svelte';
+
+  //let topnav;
+
+  // runs after the component is first rendered to the DOM
+  /*
+  onMount(async () => {
+    toogle_div = makeCMToggleControl();
+  });
+  */
+ 
+
+/*
+function makeCMToggleControl() {
+  const CMToggleControl = L.control({position: 'topright'});
+  CMToggleControl.onAdd = (map) => {
+    const div = L.DomUtil.create('div');
+    L.DomUtil.addClass(div, 'test');
+    toolbar = new CMToggle({target: div});
+    return div;
+  };
+  return CMToggleControl;
+}
+*/
+
 </script>
 
 <style>
 
+/* Add a black background color to the top navigation */
+
 #topnav {
   background-color: #27275b;
-  overflow: visible;
+  overflow: hidden;
   padding: 8px 20px;
-  display: inline-block;
 }
+
+
+/* Style the links inside the navigation bar */
 
 #topnav div {
   color: #eff4fa;
@@ -41,12 +73,6 @@ img {
   box-sizing: border-box;
 }
 
-#slot {
-  vertical-align: middle;
-  display: inline-block;
-  box-sizing: border-box;
-}
-
 
 
 </style>
@@ -55,6 +81,6 @@ img {
   <div id='logo'> <img src='images/logo.png' alt='EnerMaps'> </div>
   <span></span>
   <div id='title'>EnerMaps</div>
-  <span></span>
-  <slot id='slot'></slot>
+  <div id='cm_control'> 
+  </div>
 </div> 
