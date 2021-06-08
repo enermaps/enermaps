079f8f717b70bfdaecd82889a25e5d620113094e
Giuseppe PERONATO <giuseppe.peronato@idiap.ch>
2021-06-08 14:28:54 +0200

    fix dp name

--- data-integration/getHotMaps_raster.py
+++ data-integration/getHotMaps_raster.py
@@ -86,7 +86,7 @@ def get(repository: str, dp: frictionless.package.Package, isForced: bool = Fals
             )
             raster = {
                 "value": os.path.basename(new_dp["resources"][resource_idx]["path"]),
-                "variable": new_dp["resources"][resource_idx]["variable"],
+                "variable": new_dp["resources"][resource_idx]["title"],
                 "start_at": start_at,
                 "z": Z,
                 "unit": unit,
