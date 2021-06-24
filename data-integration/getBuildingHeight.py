5d81d219c2d7803743ca93782082e0e7c353f2e1
Giuseppe PERONATO <giuseppe.peronato@idiap.ch>
2021-06-24 22:08:45 +0200

    updating getBuildingHeight

--- data-integration/getBuildingHeight.py
+++ data-integration/getBuildingHeight.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Prepare the Building Height dataset for EnerMaps.
 
@@ -7,17 +6,18 @@ Note that the  files must be downloaded from Copernicus (requires log-in)
 and extracted in the data/31 directory.
 This script expects that the original zip file from Copernicus is extracted
 in multiple zip files.
-Created on Wed June 02 12:50:00 2021
 
 @author: giuseppeperonato
 """
 
+import argparse
 import glob
 import json
 import logging
 import os
 import pathlib
 import shutil
+import subprocess
 import sys
 
 import geopandas as gpd
@@ -32,6 +32,7 @@ ISRASTER = True
 logging.basicConfig(level=logging.INFO)
 
 DB_URL = utilities.DB_URL
+SRS = CRS.from_string("EPSG:3035")
 
 
 def getExtentBox(ds):
@@ -46,26 +47,42 @@ def getExtentBox(ds):
 
 def convertZip(directory: str):
     """Convert files downloaded from Copernicus."""
-    files_list = glob.glob(os.path.join(directory, "*.zip"))
-    if len(files_list) > 0:
+    zipfiles = glob.glob(os.path.join(directory, "*.zip"))
+    if zipfiles:
         logging.info("Extracting zip files")
-        for zipfile in files_list:
+        for zipfile in zipfiles:
             extract_dir = os.path.join(
-                os.path.dirname(zipfile), pathlib.Path(zipfile).stem
+                os.path.dirname(zipfile),
+                pathlib.Path(zipfile).stem,  # use the name of file without extension
             )
             extracted = utilities.extractZip(zipfile, extract_dir)
-            source_file = [x for x in extracted if x.endswith("tif")][0]
+            tif_files = [x for x in extracted if x.endswith("tif")]
 
+            if not tif_files:
+                logging.warning("no tiff file found in zipfile")
+                continue
+
+            source_file = tif_files[0]
             logging.info(source_file)
 
-            dest_file = os.path.join(
-                os.path.dirname(extract_dir), os.path.basename(extract_dir) + ".tif",
+            dest_file = extract_dir + ".tif"
+            subprocess.run(
+                [
+                    "gdal_translate",
+                    source_file,
+                    dest_file,
+                    "-a_srs",
+                    SRS.to_string(),
+                    "-of",
+                    "GTIFF",
+                    "--config",
+                    "GDAL_PAM_ENABLED NO",
+                    "-co COMPRESS=DEFLATE -co BIGTIFF=YES",
+                ],
+                shell=False,
             )
-            os.system(
-                "gdal_translate {source_file} {dest_file}  -a_srs EPSG:3035 -of GTIFF --config GDAL_PAM_ENABLED NO -co COMPRESS=DEFLATE -co BIGTIFF=YES".format(
-                    source_file=source_file, dest_file=dest_file
-                )
-            )
+
+            # Remove temporary files
             shutil.rmtree(extract_dir)
             os.remove(zipfile)
     else:
@@ -82,96 +99,79 @@ def get(directory):
         src_ds = gdal.Open(file)
         prj = src_ds.GetProjection()
         srs = osr.SpatialReference(wkt=prj)
-        source_crs = CRS.from_epsg(srs.GetAttrValue("authority", 1))
+        source_srs = CRS.from_epsg(srs.GetAttrValue("authority", 1))
 
-        try:
-            assert source_crs.to_string() == "EPSG:3035"
-        except AssertionError:
-            logging.error("Input files must be in EPSG:3035")
+        if source_srs != SRS:
+            logging.error("Input files must be in {}".format(SRS.to_string()))
 
         extentBox = getExtentBox(src_ds)
         fids.append(os.path.basename(file))
         extents.append(extentBox)
 
-    enermaps_data = pd.DataFrame(
-        columns=[
-            "start_at",
-            "fields",
-            "variable",
-            "value",
-            "ds_id",
-            "fid",
-            "dt",
-            "z",
-            "israster",
-            "unit",
-        ]
-    )
+    enermaps_data = utilities.ENERMAPS_DF
     enermaps_data["fid"] = fids
 
-    spatial = gpd.GeoDataFrame(geometry=extents, crs="EPSG:3035",)
+    spatial = gpd.GeoDataFrame(geometry=extents, crs=SRS.to_string(),)
     spatial["fid"] = fids
 
     return enermaps_data, spatial
 
 
 if __name__ == "__main__":
-    argv = sys.argv
-    datasets = pd.read_csv("datasets.csv", engine="python", index_col=[0])
-    ds_id = int(
-        datasets[datasets["di_script"] == os.path.basename(argv[0])].index.values[0]
-    )
-    url = datasets.loc[
-        datasets["di_script"] == os.path.basename(argv[0]), "di_URL"
-    ].values[0]
-
-    if "--force" in argv:
-        isForced = True
+    datasets = pd.read_csv("datasets.csv", index_col=[0])
+    ds_ids = datasets[datasets["di_script"] == os.path.basename(sys.argv[0])].index
+    if len(sys.argv) > 1:
+        parser = argparse.ArgumentParser(description="Import dataset")
+        parser.add_argument("--force", action="store_const", const=True, default=False)
+        parser.add_argument(
+            "--select_ds_ids", action="extend", nargs="+", type=int, default=[]
+        )
+        args = parser.parse_args()
+        isForced = args.force
+        if len(args.select_ds_ids) > 0:
+            ds_ids = args.select_ds_ids
     else:
         isForced = False
 
-    directory = "./data/{}".format(ds_id)
+    for ds_id in ds_ids:
+        directory = "./data/{}".format(ds_id)
 
-    if (
-        os.path.exists(directory)
-        and os.path.isdir(directory)
-        and len(os.listdir(directory)) == N_FILES
-    ):
-        # Dezip
-        convertZip(directory)
+        if os.path.isdir(directory) and len(os.listdir(directory)) == N_FILES:
+            # Dezip
+            convertZip(directory)
 
-        data, spatial = get(directory)
+            data, spatial = get(directory)
 
-        # Remove existing dataset
-        if utilities.datasetExists(ds_id, DB_URL) and not isForced:
-            raise FileExistsError("Use --force to replace the existing dataset.")
-        elif utilities.datasetExists(ds_id, DB_URL) and isForced:
-            utilities.removeDataset(ds_id, DB_URL)
-            logging.info("Removed existing dataset")
-        else:
-            pass
+            # Remove existing dataset
+            if utilities.datasetExists(ds_id, DB_URL) and not isForced:
+                raise FileExistsError("Use --force to replace the existing dataset.")
+            elif utilities.datasetExists(ds_id, DB_URL) and isForced:
+                utilities.removeDataset(ds_id, DB_URL)
+                logging.info("Removed existing dataset")
+            else:
+                pass
 
-        # Create dataset table
-        metadata = datasets.loc[ds_id].fillna("").to_dict()
-        metadata = json.dumps(metadata)
-        dataset = pd.DataFrame([{"ds_id": ds_id, "metadata": metadata}])
-        utilities.toPostgreSQL(
-            dataset, DB_URL, schema="datasets",
-        )
-
-        # Create data table
-        data["ds_id"] = ds_id
-        utilities.toPostgreSQL(
-            data, DB_URL, schema="data",
-        )
-        # Create spatial table
-        spatial["ds_id"] = ds_id
-        utilities.toPostGIS(
-            spatial, DB_URL, schema="spatial",
-        )
-    else:
-        logging.error(
-            "The {} directory must exist and contain {} files from Copernicus.".format(
-                directory, N_FILES
+            # Create dataset table
+            metadata = datasets.loc[ds_id].fillna("").to_dict()
+            metadata = json.dumps(metadata)
+            dataset = pd.DataFrame([{"ds_id": ds_id, "metadata": metadata}])
+            utilities.toPostgreSQL(
+                dataset, DB_URL, schema="datasets",
             )
-        )
+
+            # Create data table
+            data["ds_id"] = ds_id
+            utilities.toPostgreSQL(
+                data, DB_URL, schema="data",
+            )
+            # Create spatial table
+            spatial["ds_id"] = ds_id
+            utilities.toPostGIS(
+                spatial, DB_URL, schema="spatial",
+            )
+        else:
+            logging.error(
+                "The %s directory must exist and contain %s files from Copernicus.",
+                directory,
+                N_FILES,
+            )
