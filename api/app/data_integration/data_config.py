81ec2902d92e2de15fe81248649a45eb165f3e67
Mélanie Huck <melanie.huck@idiap.ch>
2021-07-12 14:29:15 +0200

    Adding default datasets configuration

---
+++ api/app/data_integration/data_config.py
@@ -0,0 +1,309 @@
+
+DATASETS_DIC = {
+    "DS_1" : {
+        "id" : 1,
+        "params" : """{
+            "data.ds_id":1,
+            "variable":"''Monthly average global irradiance on a horizontal surface (W/m2), period 2005-2015''",
+            "start_at":"''01-01-2099  00:00:00''"}""",
+        "legend" : "blablabla"
+    },
+    "DS_2" : {
+        "id" : 2,
+        "params" : """{"data.ds_id":2}""",
+        "legend" : "blablabla"
+    },
+    "DS_3" : {
+        "id" : 3,
+        "params" : """{"data.ds_id":3}""",
+        "legend" : "blablabla"
+    },
+    "DS_4" : {
+        "id" :  4,
+        "params" : """{"data.ds_id":4}""",
+        "legend" : "blablabla"
+    },
+    "DS_5" : {
+        "id" : 5,
+        "params" : """{
+                        "data.ds_id":5,
+                        "start_at":"''01/01/2019  00:00:00''",
+                        "fields":{
+                            "Market_Sector":"Total"
+                        }
+                    }""",
+        "legend" : "blablabla"
+    },
+    "DS_6" : {
+        "id" : 6,
+        "params" : """{
+                        "data.ds_id":6,
+                        "start_at":"''01/01/2010  00:00:00''"
+                    }""",
+        "legend" : "blablabla"
+    },
+    "DS_9" : {
+        "id" : 9,
+        "params" : """{
+                        "data.ds_id":9,
+                        "start_at":"''01/01/2010  00:00:00''",
+                        "level": " { country } "
+                    }"""
+    },
+
+    "DS_10" : {
+        "id" : 10,
+        "params" : """{
+                        "data.ds_id":10,
+                        "fields":{
+                            "action":"Buildings (residential): refurbishment"
+                        }
+                    }""",
+        "legend" : "blablabla"
+    },
+    
+    "DS_11" : {
+        "id" : 11,
+        "params" : """{
+                        "data.ds_id":11,
+                        "start_at":"''01/01/2010  00:00:00''",
+                        "fields":{
+                            "action":"Renewable energy technologies"
+                        }
+                    }""",
+        "legend" : "blablabla"
+    },
+
+    "DS_14" : {
+        "id" : 14,
+        "params" : """{
+                        "data.ds_id":14,
+                        "start_at":"''01/01/1970  00:00:00''",
+                        "variable":"''Maximum monthly 1-day precipitation total''"
+                    }""",
+        "legend" : "blablabla"
+    }, 
+    "DS_15" : {
+        "id" : 15,
+        "params" : """{
+                        "data.ds_id":15,
+                        "start_at":"''01/01/2018  00:00:00''",
+                        "variable":"''10m_u_component_of_wind''"
+                    }""",
+        "legend" : "blablabla"
+    },
+    "DS_16" : {
+        "id" : 16,
+        "params" : """{
+                        "data.ds_id":16,
+                        "start_at":"''01/01/2014  00:00:00''"
+                    }""",
+        "legend" : "blablabla"
+    },
+    "DS_17" : {
+        "id" : 17,
+        "params" : """{
+                        "data.ds_id":17,
+                        "start_at":"''01/01/2014  00:00:00''"
+                    }""",
+        "legend" : "blablabla"
+    },
+    "DS_18" : {
+        "id" : 18,
+        "params" : """{
+                        "data.ds_id":18,
+                        "start_at":"''01/01/2012  00:00:00''"
+                    }""",
+        "legend" : "blablabla"
+    },
+
+    "DS_19" : {
+        "id" : 19,
+        "params" : """{
+                    "data.ds_id":19,
+                    "start_at":"''01/01/2012  00:00:00''",
+                    "fields":{
+                        "Sector":"Power Industry"
+                    }
+                }""",
+        "legend" : "blablabla"
+    },
+    "DS_20" : {
+        "id" : 20,
+        "params" : """{
+                        "data.ds_id":20,
+                        "start_at":"''01/01/2019  00:00:00''"
+                    }""",
+        "legend" : "blablabla"
+    },
+    "DS_21" : {
+        "id" : 21,
+        "params" : """{
+                            "data.ds_id":21,
+                            "intersecting":"POLYGON((2.276722801998659 48.889240956946985,2.2747270124557986 48.835409141414466,2.390482805942611 48.847230841511724,2.3445796464564523 48.91023278929048,2.276722801998659 48.889240956946985))"
+                        }
+                    """,
+        "legend" : "blablabla"
+    },
+    "DS_22" : {
+        "id" : 22,
+        "params" : """{
+                        "data.ds_id":22,
+                        "start_at":"''01/01/2018  00:00:00''"
+                    }""",
+        "legend" : "blablabla"
+    },
+    "DS_24" : {
+        "id" : 24,
+        "params" : """{
+                        "data.ds_id":24,
+                        "start_at":"''01/01/2099  00:00:00''",
+                        "intersecting":"POLYGON((10.276722801998659 48.889240956946985,10.2747270124557986 48.835409141414466,10.390482805942611 48.847230841511724,10.3445796464564523 48.91023278929048,10.276722801998659 48.889240956946985))"
+                    }""",
+        "legend" : "blablabla"
+    },
+    "DS_27" : {
+        "id" : 27,
+        "params" : """{
+                    "data.ds_id":27,
+                    "start_at":"''01/01/2012  00:00:00''",
+                    "fields":{
+                        "parameter":"Dry Mass",
+                        "potential":"Base potential"
+                    }
+                }""",
+        "legend" : "blablalaa"
+    },
+    "DS_28" : {
+        "id" : 28,
+        "params" : """{
+                        "data.ds_id":28,
+                        "start_at":"''01/01/1945  00:00:00''",
+                        "fields":{
+                            "bage":"1945 - 1969",
+                            "btype":"Single family- Terraced houses",
+                            "detail":"insulation",
+                            "sector":"Residential sector"
+                        }
+                    }""",
+        "legend" : "blablabla"
+    },
+    "DS_29" : {
+        "id" : 29,
+        "params" : """{
+                        "data.ds_id":29,
+                        "start_at":"''2012-01-01''",
+                        "fields":{
+                            "Fuel":"gas",
+                            "Type":"space heating",
+                            "Scenario":"current",
+                            "Supertype":"Residential",
+                            "Technology":"Gas boiler or stove"
+                        }
+                    }""",
+        "legend" : "blablabla"
+    },
+    "DS_30" : {
+        "id" : 30,
+        "params" : """{
+                    "data.ds_id":30,
+                    "start_at":"''2012-01-01''",
+                    "fields":{
+                        "Sector":"Industry",
+                        "Scenario":"CP",
+                        "Sub-sector":"TOTAL Industry",
+                        "Energy type":"Final Energy",
+                        "Energy Carrier":"Total"
+                    }
+                }""",
+        "legend" : "blablabla"
+    },
+    "DS_31" : {
+        "id" : 31,
+        "params" : """{
+                        "data.ds_id":31,
+                        "variable":"''Climate zones''"
+                    }""",
+        "legend" : "blablabla"
+    },
+    "DS_33" : {
+        "id" : 33,
+        "params" : """{
+                        "data.ds_id":33,
+                        "intersecting":"POLYGON((2.276722801998659 48.889240956946985,2.2747270124557986 48.835409141414466,2.390482805942611 48.847230841511724,2.3445796464564523 48.91023278929048,2.276722801998659 48.889240956946985))"
+                    }""",
+        "legend" : "blablabla"
+    },
+    "DS_35" : {
+        "id" : 35,
+        "params" : """{
+                        "data.ds_id":35,
+                        "intersecting":"POLYGON((2.29 48.88,2.29 48.87,2.3 48.87,2.3 48.88,2.29 48.88))"
+                    }""",
+        "legend" : "blablabla"
+    },
+    "DS_42" : {
+        "id" : 42,
+        "params" : """{
+                        "data.ds_id":42,
+                        "level":"{country}"
+                    }""",
+        "legend" : "blablabla"
+    },
+    "DS_43" : {
+        "id" : 43,
+        "params" : """{
+                        "data.ds_id":43
+                    }""",
+        "legend" : "blablabla"
+    },
+    "DS_45" : {
+        "id" : 45,
+        "params" : """{"data.ds_id":45}""",
+        "legend" : "blablabla"
+    },
+    "DS_46" : {
+        "id" : 46,
+        "params" : """{
+                        "data.ds_id":46,
+                        "start_at":"''2018-01-01''",
+                        "fields":{
+                            "Pollutant":"Carbon dioxide"
+                        }
+                    }
+                    """,
+        "legend" : "blablabla"
+    },
+    "DS_47" : {
+        "id" : 47,
+        "params" : """{
+                        "data.ds_id":47,
+                        "start_at":"''2018-01-01''"
+                    }""",
+        "legend" : "blablabla"
+    },
+    "DS_48" : {
+        "id" : 48,
+        "params" : """{
+                        "data.ds_id":48,
+                        "start_at":"''2018-01-01''"
+                    }""",
+        "legend" : "blablabla"
+    },
+    "DS_49" : {
+        "id" : 49,
+        "params" : """{
+                        "data.ds_id":49,
+                        "start_at":"''2018-01-01''"
+                    }""",
+        "legend" : "blablabla"
+    },
+    "DS_50" : {
+        "id" : 50,
+        "params" : """{
+                    "data.ds_id":50,
+                    "start_at":"''2018-01-01''"
+                }""",
+        "legend" : "blablabla"
+    }
+}
