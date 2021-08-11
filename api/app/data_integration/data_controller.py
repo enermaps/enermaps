b0f844a8398c12b54572caec386ea5d418d128fc
Mélanie Huck <melanie.huck@idiap.ch>
2021-08-11 06:18:21 +0200

    Uncommenting nuts

--- api/app/data_integration/data_controller.py
+++ api/app/data_integration/data_controller.py
@@ -5,22 +5,20 @@ from app.models.geofile import create
 def init_enermaps_datasets():
 
     # Get NUTS and LAU datasets
-    # nuts_and_lau_datasets = ["country", "NUTS1", "NUTS2", "NUTS3", "LAU"]
-    # for dataset_name in nuts_and_lau_datasets:
-    #     try:
-    #         file_upload = enermaps_server.get_nuts_and_lau_dataset(dataset_name)
-    #         if file_upload is not None:
-    #             create(file_upload)
-    #     except Exception as e:
-    #         print("Error creating dataset {}".format(dataset_name))
-    #         print(e)
-
-    # [21, 24, 33, 35] : These datasets are tiled raster datasets (needing input coordinates)
-    #                    and returning multiple images
+    nuts_and_lau_datasets = ["country", "NUTS1", "NUTS2", "NUTS3", "LAU"]
+    for dataset_name in nuts_and_lau_datasets:
+        try:
+            file_upload = enermaps_server.get_nuts_and_lau_dataset(dataset_name)
+            if file_upload is not None:
+                create(file_upload)
+        except Exception as e:
+            print("Error creating dataset {}".format(dataset_name))
+            print(e)
 
     # Get the ids of the datasets that we want to load
     datasets_ids = data_endpoints.get_ds_ids()
-    datasets_ids = [5]
+    # To download only a subset of the datasets (!datasets ids must be in the config file!)
+    # datasets_ids = [1,2,3,4,5,6]
 
     # Check that the datasets that we want to load are in the enermaps DB
     for dataset_id in datasets_ids:
