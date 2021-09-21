4a0ea5208ace5d9d8e5f59ee0a11800f0bcfe524
Philip ABBET <philip.abbet@idiap.ch>
2021-09-21 15:05:11 +0200

    Fix the remaining linting errors (from bandit)

--- api/main.py
+++ api/main.py
@@ -4,7 +4,7 @@ For running this, you will need to have a local install
 of all packages in requirements.txt. You can also
 refer to the Dockerfile for the list of commands to run.
 
-Due to a bug in flask package resulution, you will need to
+Due to a bug in flask package resolution, you will need to
 run this script with the main.py as an absolute path, for example:
 
 python $(pwd)/main.py
@@ -14,4 +14,4 @@ from app import create_app
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0", port=7000, debug=True, threaded=False)
+    app.run(host="127.0.0.1", port=7000, debug=True, threaded=False)
