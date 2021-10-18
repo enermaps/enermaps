da48cd395e4dfcbd43ed6c78fba1450434481c65
Philip ABBET <philip.abbet@idiap.ch>
2021-10-18 15:34:48 +0200

    Adapt to the latest changes related to database usage

--- cm/heatlearn/test.py
+++ cm/heatlearn/test.py
@@ -18,19 +18,32 @@ def load_geojson(test_filename):
         return json.load(fd)
 
 
+class MockTask:
+    def __init__(self):
+        self.nb_rasters_posted = 0
+
+    def post_raster(self, raster_name, raster_fd):
+        self.nb_rasters_posted += 1
+
+
 class TestCM(unittest.TestCase):
     def test_rasterstats(self):
         selection = load_geojson("ge_rive_droite_500.geojson")
         raster_paths = [get_testdata_path("200km_2p5m_N26E38_07_18.tif")]
+
+        task = MockTask()
+
         stats = heatlearn.heatlearn(
-            selection, raster_paths, 500, 2020, to_colorize=False
+            task, selection, raster_paths, 500, 2020, to_colorize=False
         )
 
         self.assertEqual(
             stats["values"]["Annual heating demand [GWh]"],
             148,
-            "Request especting 0" " returned different values",
+            "Request especting 0 returned different values",
         )
+
+        self.assertEqual(task.nb_rasters_posted, 1)
 
 
 if __name__ == "__main__":
