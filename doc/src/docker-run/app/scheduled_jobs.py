4ed186c7d01c5d452a80f9672d93860d2f94d16c
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2021-11-08 19:42:53 +0100

    migrate to using ubuntu:20.04 & gollum:5.2.3 for the doc platform

--- doc/src/docker-run/app/scheduled_jobs.py
+++ doc/src/docker-run/app/scheduled_jobs.py
@@ -37,4 +37,4 @@ if __name__ == '__main__':
 
     while True:
         schedule.run_pending()
-        time.sleep(30)
+        time.sleep(5)
--- doc/src/gollum-data/gollum-patch/js/custom.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-  console.log("DOM fully loaded and parsed");
-});
