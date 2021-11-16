09944958db30e2ab9610a5d14deffc8a76e8f4c7
Pietro Zambelli <pietro.zambelli@eurac.edu>
2021-11-16 11:02:49 +0100

    First draft of the refurbish cm

---
+++ cm/cm_refurbish/test.py
@@ -0,0 +1,27 @@
+import json
+import os
+import unittest
+
+# from refurbish import ref_rate
+
+CURRENT_FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_testdata_path(filename):
+    """Return the absolute path of the filename."""
+    return os.path.join(CURRENT_FILE_DIR, "testdata", filename)
+
+
+def load_geojson(test_filename):
+    test_geojson = get_testdata_path(test_filename)
+    with open(test_geojson) as fd:
+        return json.load(fd)
+
+
+class TestCM(unittest.TestCase):
+    def test_rasterstats(self):
+        ...
+
+
+if __name__ == "__main__":
+    unittest.main()
