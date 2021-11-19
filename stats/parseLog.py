f09faa63e62c84e91b734d3fe0a1256ed243a060
Giuseppe PERONATO <giuseppe.peronato@idiap.ch>
2021-11-19 17:52:27 +0100

    update logic

--- stats/parseLog.py
+++ stats/parseLog.py
@@ -41,9 +41,7 @@ def parseLog(log_file: str, parsed_log_file: str):
         "backend_type",
     ]
     try:
-        log = pd.read_csv(
-            log_file, header=None, error_bad_lines=False, low_memory=False
-        )
+        log = pd.read_csv(log_file, header=None, on_bad_lines="skip", low_memory=False)
         log.columns = header
     except (pd.errors.EmptyDataError, pd.errors.ParserError):
         print("Cannot decode the content of the log file")
@@ -67,10 +65,11 @@ def parseLog(log_file: str, parsed_log_file: str):
             log["message"]
             .str.lower()
             .str.startswith("statement: select * from enermaps"),
-            ["log_time", "message"],
+            :,
         ]
+
         if queries.shape[0] > 0:
-            queries["message"] = log["message"].str.replace("\n", "")
+            queries["message"] = queries["message"].str.replace("\n", "")
             # Get the query
             queries["json_query"] = queries.message.str.extract(r"(?<=\(')(.*)(?='\))")
             queries["json_query"] = queries["json_query"].apply(
@@ -94,8 +93,11 @@ def parseLog(log_file: str, parsed_log_file: str):
         # Parse log
         # PostgREST queries
         queries = log.loc[
-            ~log["detail"].isnull(),  # the parameters are saved in the detail field
-            ["log_time", "message", "detail", "session_id", "session_line_num"],
+            (~log["detail"].isnull())
+            & (
+                log["message"].str.startswith("execute ")
+            ),  # the parameters are saved in the detail field
+            :,
         ]
         for string in QUERY_STRINGS:
             queries = queries.loc[queries.message.str.contains(string), :]
@@ -112,7 +114,10 @@ def parseLog(log_file: str, parsed_log_file: str):
             # Get the dataset_id
             ds_id = []
             for d in queries.json_query:
-                parameters = d.get("parameters", {})
+                if "id" in d.keys():
+                    parameters = d
+                else:
+                    parameters = d.get("parameters", {})
                 if isinstance(parameters, str):
                     parameters = json.loads(parameters)
                 if "data.ds_id" in parameters.keys():
@@ -123,14 +128,15 @@ def parseLog(log_file: str, parsed_log_file: str):
                     ds_id.append(-1)
             queries["ds_id"] = ds_id
             # Drop duplicate rows
-            queries = queries.groupby("session_id").first()
+            queries = queries.groupby("log_time").first().reset_index()
 
             # Find PostGrest function
             queries["function"] = queries["message"].str.extract(
                 r'(?<=SELECT "public".")(.*)(?="\()'
             )
+
             # Find geolocalization info based on session_id
-            geolocal = log.loc[log["session_id"].isin(queries.index), :]
+            geolocal = log.loc[log["session_id"].isin(queries["session_id"]), :]
             # Get the IP address
             geolocal = geolocal.loc[
                 geolocal["message"].str.contains("request.header.x-forwarded-for"), :
@@ -143,7 +149,10 @@ def parseLog(log_file: str, parsed_log_file: str):
             # Merge geolocalization info with query info
             if geolocal.shape[0] > 0:
                 queries = pd.merge(
-                    geolocal[["ip", "session_id"]], queries, on="session_id"
+                    geolocal[["ip", "session_id"]],
+                    queries,
+                    on="session_id",
+                    how="outer",
                 )
             else:
                 queries["ip"] = None
@@ -175,31 +184,35 @@ def parseLog(log_file: str, parsed_log_file: str):
             parsed_log[sel_cols].to_csv(
                 parsed_log_file, mode="a", header=None, index=False
             )
+            return parsed_log
 
 
 if __name__ == "__main__":
-    print("calling parsing")
-    parser = argparse.ArgumentParser(description="Create log")
-    parser.add_argument("source_log_file", default="all")
-    parser.add_argument("--parsed_log_file", "-o", default="tmp.csv", required=False)
+    log_file = "pglog_0800.csv"
+    parsed_log_file = "test.csv"
+    parsed_log = parseLog(log_file, parsed_log_file)
+    # print("calling parsing")
+    # parser = argparse.ArgumentParser(description="Create log")
+    # parser.add_argument("source_log_file", default="all")
+    # parser.add_argument("--parsed_log_file", "-o", default="tmp.csv", required=False)
 
-    args = parser.parse_args()
+    # args = parser.parse_args()
 
-    if args.source_log_file == "all":
-        # By default parse all the log files but the last one and remove them
-        for log_file in sorted(
-            glob.glob("/db-data/pg_log/*.csv"), key=os.path.getmtime
-        )[:-1]:
-            print("Reading {}".format(log_file))
-            parseLog(log_file, os.path.abspath(os.path.join("stats", "parsed_log.csv")))
-            # Remove source log files
-            os.remove(log_file)
-            log_file2 = log_file.replace(".csv", "")
-            if os.path.exists(log_file2):
-                os.remove(log_file2)
-    else:
-        print("Manually loading log file")
-        parseLog(
-            os.path.abspath(os.path.join("db-data", "pg_log", args.source_log_file)),
-            os.path.abspath(os.path.join("stats", args.parsed_log_file)),
-        )
+    # if args.source_log_file == "all":
+    #     # By default parse all the log files but the last one and remove them
+    #     for log_file in sorted(
+    #         glob.glob("/db-data/pg_log/*.csv"), key=os.path.getmtime
+    #     )[:-1]:
+    #         print("Reading {}".format(log_file))
+    #         parseLog(log_file, os.path.abspath(os.path.join("stats", "parsed_log.csv")))
+    #         # Remove source log files
+    #         os.remove(log_file)
+    #         log_file2 = log_file.replace(".csv", "")
+    #         if os.path.exists(log_file2):
+    #             os.remove(log_file2)
+    # else:
+    #     print("Manually loading log file")
+    #     parseLog(
+    #         os.path.abspath(os.path.join("db-data", "pg_log", args.source_log_file)),
+    #         os.path.abspath(os.path.join("stats", args.parsed_log_file)),
+    #     )
