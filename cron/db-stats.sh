d687e97de1e51492d2572475d37f2f4591cb92b7
Giuseppe PERONATO <giuseppe.peronato@idiap.ch>
2021-11-30 17:51:04 +0100

    fix cronjob

--- cron/db-stats.sh
+++ cron/db-stats.sh
@@ -4,4 +4,4 @@
 export $(grep -v '^#' /etc/enermaps/config | xargs)
 
 # Parse database logs
-echo $(date -u) && docker-compose -f $ENERMAPS_ROOT/docker-compose-db.yml run --rm stats python3 parseLog.py all
+echo $(date -u) && docker-compose -f $ENERMAPS_ROOT/docker-compose-db.yml run --rm stats python3 parseLogs.py --sql --remove --skip_last
