f791a12c01af01be99b794a9367e9054612e4254
Philip ABBET <philip.abbet@idiap.ch>
2021-12-14 13:32:02 +0100

    Move the content of the 'About' dialog in its own file

---
+++ frontend/src/src/popups/About.svelte
@@ -0,0 +1,12 @@
+<p>
+  EnerMaps Open Data Management Tool aims to improve data management and
+  accessibility in the field of energy research for the renewable energy industry.
+</p>
+<p>
+  EnerMaps’ tool accelerates and facilitates the energy transition offering a
+  qualitative and user-friendly digital platform to the energy professionals.
+</p>
+<p>
+  The EnerMaps project has received funding from the European Union’s Horizon
+  2020 research and innovation program under grant agreement N°884161.
+</p>
