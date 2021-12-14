3fdb0b37e5e24fabd45092ab4ff8e8633e2f010f
Philip ABBET <philip.abbet@idiap.ch>
2021-12-14 15:33:24 +0100

    Update the content of the about dialog

--- frontend/src/src/popups/About.svelte
+++ frontend/src/src/popups/About.svelte
@@ -1,12 +1,77 @@
+<style>
+  .nolist {
+    list-style-type: none;
+    margin-top: 8px;
+  }
+
+  .nolist li {
+    margin-top: 6px;
+  }
+
+  .supported-browser {
+    width: 41px;
+    vertical-align: middle;
+    margin-right: 12px;
+  }
+
+  .europe-logo {
+    height: 100px;
+    margin-bottom: 10px;
+  }
+
+  .europe-logo img {
+    height: 100%;
+    margin-right: 10px;
+    float: left;
+  }
+</style>
+
+
 <p>
-  EnerMaps Open Data Management Tool aims to improve data management and
+  The EnerMaps Open Data Management Tool aims to improve data management and
   accessibility in the field of energy research for the renewable energy industry.
 </p>
+
 <p>
   EnerMaps’ tool accelerates and facilitates the energy transition offering a
   qualitative and user-friendly digital platform to the energy professionals.
 </p>
+
 <p>
-  The EnerMaps project has received funding from the European Union’s Horizon
-  2020 research and innovation program under grant agreement N°884161.
+  More information on the project can be found here:
+  <a href="https://enermaps.eu/">https://enermaps.eu/</a>
 </p>
+
+<p>
+  <strong>EnerMaps Wiki:</strong> For details about tool functionalities as well as
+  the manual for using the calculation modules, please refer to the EnerMaps Wiki:
+  <a href="https://enermaps-wiki.herokuapp.com/">https://enermaps-wiki.herokuapp.com/</a>
+</p>
+
+<p>
+  <strong>Open source:</strong> The developed tool and all related modules run without
+  requiring any other commercial tool or software. Use of and access to Source Code is
+  subject to Open Source Licenses. The source code of the tool can be accessed via
+  <a href="https://www.github.com/enermaps/">https://www.github.com/enermaps</a>
+</p>
+
+<div>
+  <strong>Supported browsers:</strong>
+  <ul class="nolist">
+    <li><img class="supported-browser" src="images/firefox.png" alt="firefox"> <span class="browser-label">Firefox version 92+</span></li>
+    <li><img class="supported-browser" src="images/chrome.png" alt="chrome"> <span class="browser-label">Chrome version 96+</span></li>
+  </ul>
+</div>
+
+<p>
+  <strong>Disclaimer:</strong> The data and tools provided on this website are
+  indicative and for research purposes only. No responsibility is taken for the
+  accuracy of included data, figures and results or for using them for unintended
+  purposes.
+</p>
+
+<div class="europe-logo">
+  <img src="images/european_flag.png" alt="European flag">
+  <span>The EnerMaps project has received funding from the European Union’s Horizon
+  2020 research and innovation program under grant agreement N°884161.</span>
+</div>
