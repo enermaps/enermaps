c38b0a3a446e1f3ad8d9b057634bd89f3340c9a0
Cédric M. Serugendo <cedric.serugendo@crem.ch>
2022-04-21 12:05:21 +0200

    change 'Area selection' to 'Region selection'

--- frontend/src/src/components/AreaSelection.svelte
+++ frontend/src/src/components/AreaSelection.svelte
@@ -67,7 +67,7 @@
   {#if !availableAreas}
     Loading areas...
   {:else}
-    <label for="id">Area selection:</label>
+    <label for="id">Region selection:</label>
 
     <select id="areas" class="areas" bind:value={$areaSelectionStore}>
       {#each availableAreas as area}
